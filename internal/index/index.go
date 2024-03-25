dfb951516a72abc97e0343d947bec1adc80032e4
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-03-25 08:41:58 +0200

    Initial commit of `index` package
    The `index` package provides the index key used to lookup PersistentVolumeClaim
    objects, as well as an IndexerFunc, which knows how to extract values for the
    index key.

---
+++ internal/index/index.go
@@ -0,0 +1,27 @@
+package index
+
+import (
+	"github.com/gardener/pvc-autoscaler/internal/annotation"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Key is the index key we use to lookup PersistentVolumeClaim objects.
+const Key = "pvc.autoscaling.gardener.cloud/idx"
+
+// IndexerFunc is a [sigs.k8s.io/controller-runtime/pkg/client.IndexerFunc],
+// which knows how to extract values for [Key] index.
+func IndexerFunc(rawObj client.Object) []string {
+	obj, ok := rawObj.(*corev1.PersistentVolumeClaim)
+	if !ok {
+		return []string{}
+	}
+
+	value, ok := obj.Annotations[annotation.Enable]
+	if !ok {
+		return []string{}
+	}
+
+	return []string{value}
+}
