98fb4be1ea011eba9596f12fc19847be774da853
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-03-26 11:56:04 +0200

    annotation: add annotations for increase-by, threshold and max-capacity

--- internal/annotation/annotation.go
+++ internal/annotation/annotation.go
@@ -6,5 +6,21 @@ const (
 
 	// IsEnabled is the annotation used to specify that autoscaling is
 	// enabled for the PVC
-	IsEnabled = Prefix + "enable"
+	IsEnabled = Prefix + "is-enabled"
+
+	// IncreaseBy is an annotation, which specifies an increase by
+	// percentage value (e.g. 10%, 20%, etc.) by which the Persistent Volume
+	// Claim storage will be resized.
+	IncreaseBy = Prefix + "increase-by"
+
+	// Threshold is an annotation which specifies the threshold value in
+	// percentage (e.g. 10%, 20%, etc.) for the PVC. Once the available
+	// capacity for the PVC reaches or exceeds the specified threshold this
+	// will trigger a resize operation by the controller.
+	Threshold = Prefix + "threshold"
+
+	// MaxCapacity is an annotation which specifies the maximum capacity up
+	// to which a PVC is allowed to be extended. The max capacity is
+	// specified as a [k8s.io/apimachinery/pkg/api/resource.Quantity] value.
+	MaxCapacity = Prefix + "max-capacity"
 )
