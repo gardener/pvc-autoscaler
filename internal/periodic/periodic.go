64ce5dbc56509ab8ec8a1d23a1aefaa41cc017bc
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-03-27 12:40:48 +0200

    periodic: commit the known errors we will be returning

--- internal/periodic/periodic.go
+++ internal/periodic/periodic.go
@@ -2,23 +2,59 @@ package periodic
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strconv"
 	"time"
 
+	"github.com/gardener/pvc-autoscaler/internal/annotation"
 	"github.com/gardener/pvc-autoscaler/internal/index"
+	"github.com/gardener/pvc-autoscaler/internal/metrics/source"
+	"github.com/gardener/pvc-autoscaler/internal/utils"
 
 	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
+// ErrNoMetrics is an error which is returned when metrics about a PVC are
+// missing.
+var ErrNoMetrics = errors.New("no metrics found")
+
+// ErrNoMaxCapacity is an error which is returned when a PVC does not specify
+// the max capacity.
+var ErrNoMaxCapacity = errors.New("no max capacity specified")
+
+// ErrBadPercentageValue is an error which is returned when attempting to parse
+// a bad percentage value.
+var ErrBadPercentageValue = errors.New("bad percentage value")
+
+// ErrVolumeModeIsNotFilesystem is an error which is returned if a target PVC
+// for resizing is not using the Filesystem VolumeMode.
+var ErrVolumeModeIsNotFilesystem = errors.New("volume mode is not filesystem")
+
+// ErrStorageClassNotFound is an error which is returned when the storage class
+// for a PVC is not found.
+var ErrStorageClassNotFound = errors.New("no storage class found")
+
+// ErrStorageClassDoesNotSupportExpansion is an error which is returned when an
+// annotated PVC uses a storage class that does not support volume expansion.
+var ErrStorageClassDoesNotSupportExpansion = errors.New("storage class does not support expansion")
+
 // Runner is a [sigs.k8s.io/controller-runtime/pkg/manager.Runnable], which
 // enqueues PersistentVolumeClaims for reconciling on regular basis.
 type Runner struct {
-	client   client.Client
-	interval time.Duration
-	eventCh  chan event.GenericEvent
+	client        client.Client
+	interval      time.Duration
+	eventCh       chan event.GenericEvent
+	metricsSource source.Source
 }
 
 var _ manager.Runnable = &Runner{}
