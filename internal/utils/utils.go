51697bc2ead8eb32e96b1d653aa48ac36b46d4f2
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-03-27 16:44:00 +0200

    Move some common consts to `common` package
    Also, utils.ParsePercentage() will return ErrBadPercentageValue error

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,11 +1,16 @@
 package utils
 
 import (
+	"errors"
 	"strconv"
 	"strings"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+// ErrBadPercentageValue is an error which is returned when attempting to parse
+// a bad percentage value.
+var ErrBadPercentageValue = errors.New("bad percentage value")
 
 // ParsePercentage parses a string value, which represents percentage, e.g. 10%.
 func ParsePercentage(s string) (float64, error) {
@@ -13,7 +18,11 @@ func ParsePercentage(s string) (float64, error) {
 	s = strings.TrimRight(s, "%")
 	val, err := strconv.ParseFloat(s, 64)
 
-	return val, err
+	if err != nil {
+		return val, ErrBadPercentageValue
+	}
+
+	return val, nil
 }
 
 // GetAnnotation returns the annotation with the given name from the object, if
