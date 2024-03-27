f7d3c22c3ab5e6216d641823ca94119f33399f7b
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-03-27 11:22:00 +0200

    utils: commit utility function GetAnnotation()

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"strconv"
 	"strings"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // ParsePercentage parses a string value, which represents percentage, e.g. 10%.
@@ -12,4 +14,15 @@ func ParsePercentage(s string) (float64, error) {
 	val, err := strconv.ParseFloat(s, 64)
 
 	return val, err
+}
+
+// GetAnnotation returns the annotation with the given name from the object, if
+// it exists, otherwise it returns a default value.
+func GetAnnotation(obj client.Object, name, defaultVal string) string {
+	val, ok := obj.GetAnnotations()[name]
+	if !ok {
+		return defaultVal
+	}
+
+	return val
 }
