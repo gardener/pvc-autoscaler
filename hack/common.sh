d999e9f534ac5584b6a5de657b2c1461b1d43861
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-04-08 16:00:07 +0300

    hack: commit utility function to get and ensure PVC capacity

--- hack/common.sh
+++ hack/common.sh
@@ -37,12 +37,14 @@ function _msg_error() {
 # $1: event type (e.g. Normal or Warning)
 # $2: event reason
 # $3: object (e.g. pod/sample-pod)
+# $4: poll interval (defaults to 10)
+# $5: max attempts (defaults to 60)
 function _wait_for_event() {
   local _type="${1}"
   local _reason="${2}"
   local _object="${3}"
-  local _poll_sec=10
-  local _max_attempts=60
+  local _poll_sec=${4:-10}
+  local _max_attempts=${5:-60}
 
   for i in $( seq 1 "${_max_attempts}" ); do
     _msg_info "[${i}/${_max_attempts}] waiting for '${_reason}' (${_type}) event(s) ..."
@@ -70,13 +72,15 @@ function _wait_for_event() {
 # $2: event reason
 # $3: object (e.g. pod/sample-pod)
 # $4: expected number of times event has occurred
+# $5: poll interval (defaults to 10)
+# $6: max attempts (defaults to 60)
 function _wait_for_event_to_occur_n_times() {
   local _type="${1}"
   local _reason="${2}"
   local _object="${3}"
   local _n="${4}"
-  local _poll_sec=10
-  local _max_attempts=60
+  local _poll_sec=${5:-10}
+  local _max_attempts=${6:-60}
 
   for i in $( seq 1 "${_max_attempts}" ); do
     _msg_info "[${i}/${_max_attempts}] waiting for '${_reason}' (${_type}) event to occur ${_n} time(s) ..."
@@ -97,4 +101,32 @@ function _wait_for_event_to_occur_n_times() {
   done
 
   _msg_error "did not receive any '${_reason}' event(s)" 1
+}
+
+# Returns the size of a given PVC
+#
+# $1: pvc name
+# $2: namespace (defaults to "default")
+function _pvc_capacity() {
+  local _pvc_name="${1}"
+  local _namespace=${2:-default}
+
+  kubectl --namespace "${_namespace}" get pvc "${_pvc_name}" -o yaml | \
+    yq '.status.capacity.storage'
+}
+
+# Ensure that the size of the given PVC matches the given value
+#
+# $1: pvc name
+# $2: namespace
+# $3: capacity
+function _ensure_pvc_capacity() {
+  local _pvc_name="${1}"
+  local _namespace="${2}"
+  local _want_capacity="${3}"
+  local _got_capacity=$( _pvc_capacity "${_pvc_name}" "${_namespace}" )
+
+  if [ "${_want_capacity}" != "${_got_capacity}" ]; then
+    _msg_error "pvc ${_namespace}/${_pvc_name} capacity is ${_got_capacity} (want ${_want_capacity})" 1
+  fi
 }
