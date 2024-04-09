e9bb419c28abc1e7d45227c460f9c1b904fab12e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-04-09 15:30:13 +0300

    Add license headers

--- hack/common.sh
+++ hack/common.sh
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # Common shell utilities
 
 _SCRIPT_NAME="${0##*/}"
