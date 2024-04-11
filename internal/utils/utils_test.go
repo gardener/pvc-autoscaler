c4d8e29f63f0e5d7128bb90487b62757eb11be29
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-04-11 11:51:33 +0300

    utils: add tests for GetAnnotation

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -5,6 +5,9 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/gardener/pvc-autoscaler/internal/utils"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Utils", func() {
@@ -28,6 +31,27 @@ var _ = Describe("Utils", func() {
 				_, err := utils.ParsePercentage(val)
 				Expect(err).To(MatchError(utils.ErrBadPercentageValue))
 			}
+		})
+	})
+
+	Context("# GetAnnotation", func() {
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "sample-pod",
+				Annotations: map[string]string{
+					"foo": "bar",
+					"baz": "qux",
+				},
+			},
+		}
+
+		It("should return annotation from object", func() {
+			Expect(utils.GetAnnotation(pod, "foo", "")).To(Equal("bar"))
+			Expect(utils.GetAnnotation(pod, "baz", "")).To(Equal("qux"))
+		})
+		It("should return default values", func() {
+			Expect(utils.GetAnnotation(pod, "unknown", "foobar")).To(Equal("foobar"))
+			Expect(utils.GetAnnotation(pod, "missing", "default")).To(Equal("default"))
 		})
 	})
 })
