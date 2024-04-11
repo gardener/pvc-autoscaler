f20c09e8122f7e0f214f56b1bf96e58a0bb3172d
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-04-11 14:13:25 +0300

    utils: add test cases for ValidatePersistentVolumeClaimAnnotations

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -4,9 +4,12 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"github.com/gardener/pvc-autoscaler/internal/annotation"
+	"github.com/gardener/pvc-autoscaler/internal/common"
 	"github.com/gardener/pvc-autoscaler/internal/utils"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -94,5 +97,179 @@ var _ = Describe("Utils", func() {
 			Expect(utils.IsPersistentVolumeClaimConditionPresentAndEqual(pvc, corev1.PersistentVolumeClaimResizing, corev1.ConditionTrue)).To(BeFalse())
 			Expect(utils.IsPersistentVolumeClaimConditionPresentAndEqual(pvc, corev1.PersistentVolumeClaimVolumeModifyVolumeError, corev1.ConditionTrue)).To(BeFalse())
 		})
+	})
+
+	Context("# ValidatePersistentVolumeClaimAnnotations", func() {
+		It("shoud succeed with good annotations", func() {
+			pvc := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:            "true",
+						annotation.IncreaseBy:           "25%",
+						annotation.Threshold:            "25%",
+						annotation.MaxCapacity:          "100Gi",
+						annotation.LastCheck:            "1712826720",
+						annotation.NextCheck:            "1712826721",
+						annotation.UsedSpacePercentage:  "10.00%",
+						annotation.FreeSpacePercentage:  "90.00%",
+						annotation.UsedInodesPercentage: "5.00%",
+						annotation.FreeInodesPercentage: "95.00%",
+						annotation.PrevSize:             "90Gi",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc)).To(Succeed())
+		})
+
+		It("shoud fail with zero capacity", func() {
+			pvc := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:   "true",
+						annotation.MaxCapacity: "100Gi",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("0Gi"),
+					},
+				},
+			}
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc)).ShouldNot(Succeed())
+		})
+
+		It("should fail with invalid max-capacity annotation", func() {
+			pvc1 := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						// No max capacity specified
+						annotation.IsEnabled: "true",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+			pvc2 := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:   "true",
+						annotation.MaxCapacity: "foobar", // Bad resource value
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc1)).To(MatchError(common.ErrNoMaxCapacity))
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc2)).ShouldNot(Succeed())
+		})
+
+		It("should fail with bad increase-by annotation", func() {
+			pvc := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:   "true",
+						annotation.MaxCapacity: "100Gi",
+						annotation.IncreaseBy:  "bad-increase-by",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc)).ShouldNot(Succeed())
+		})
+
+		It("should fail with zero increase-by annotation", func() {
+			pvc := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:   "true",
+						annotation.IncreaseBy:  "0%",
+						annotation.MaxCapacity: "100Gi",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc)).To(MatchError(common.ErrZeroPercentage))
+		})
+
+		It("should fail with invalid threshold annotation", func() {
+			pvc1 := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:   "true",
+						annotation.Threshold:   "0%", // Zero threshold is invalid
+						annotation.MaxCapacity: "100Gi",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+			pvc2 := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:   "true",
+						annotation.Threshold:   "foobar", // Bad threshold
+						annotation.MaxCapacity: "100Gi",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc1)).To(MatchError(common.ErrZeroPercentage))
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc2)).ShouldNot(Succeed())
+
+		})
+
+		It("should fail with max capacity less than current capacity", func() {
+			pvc := &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "sample-pvc",
+					Annotations: map[string]string{
+						annotation.IsEnabled:   "true",
+						annotation.MaxCapacity: "50Gi",
+					},
+				},
+				Status: corev1.PersistentVolumeClaimStatus{
+					Capacity: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("100Gi"),
+					},
+				},
+			}
+			Expect(utils.ValidatePersistentVolumeClaimAnnotations(pvc)).ShouldNot(Succeed())
+		})
+
 	})
 })
