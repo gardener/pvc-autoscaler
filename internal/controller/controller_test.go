c4d30c8aad1a408156dbc72cd527588dffb61996
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-04-12 09:05:22 +0300

    controller/tests: add test case for pvc being resized

--- internal/controller/controller_test.go
+++ internal/controller/controller_test.go
@@ -160,5 +160,47 @@ var _ = Describe("PersistentVolumeClaim Controller", func() {
 			Expect(buf.String()).To(ContainSubstring("refusing to proceed with reconciling"))
 			Expect(k8sClient.Delete(newCtx, pvc)).To(Succeed())
 		})
+
+		It("should skip reconcile if pvc is resizing", func() {
+			ctx := context.Background()
+			pvc, err := createPvc(ctx, "pvc-is-resizing", "1Gi")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(pvc).NotTo(BeNil())
+
+			annotations := map[string]string{
+				annotation.IsEnabled:   "true",
+				annotation.MaxCapacity: "100Gi",
+			}
+			Expect(annotatePvc(ctx, pvc, annotations)).To(Succeed())
+
+			// Add the status conditions
+			patch := client.MergeFrom(pvc.DeepCopy())
+			pvc.Status.Conditions = []corev1.PersistentVolumeClaimCondition{
+				{
+					Type:   corev1.PersistentVolumeClaimResizing,
+					Status: corev1.ConditionTrue,
+				},
+			}
+			Expect(k8sClient.Status().Patch(ctx, pvc, patch)).To(Succeed())
+
+			// We should see this PVC being skipped because it is resizing
+			req := ctrl.Request{NamespacedName: client.ObjectKeyFromObject(pvc)}
+			reconciler, err := newReconciler()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(reconciler).NotTo(BeNil())
+
+			// Inspect the log messages
+			var buf strings.Builder
+			w := io.MultiWriter(GinkgoWriter, &buf)
+			logger := zap.New(zap.WriteTo(w))
+			newCtx := log.IntoContext(ctx, logger)
+
+			result, err := reconciler.Reconcile(newCtx, req)
+			Expect(result).To(Equal(ctrl.Result{}))
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(buf.String()).To(ContainSubstring("resize has been started"))
+			Expect(k8sClient.Delete(newCtx, pvc)).To(Succeed())
+		})
 	})
 })
