0126d2b631541450e4a24f7e7a123b83d477ac6f
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-11-04 11:50:23 +0200

    hack/run-e2e-tests: fix e2e tests after introducing CRD

--- hack/run-e2e-tests.sh
+++ hack/run-e2e-tests.sh
@@ -19,20 +19,29 @@ source "${_SCRIPT_DIR}/common.sh"
 # autoscaler to resize the volume until we fully exhaust the space.
 function _test_consume_space_and_resize() {
   local _pvc_yaml="${_TEST_MANIFESTS_DIR}/pvc-1.yaml"
+  local _pvca_yaml="${_TEST_MANIFESTS_DIR}/pvca-1.yaml"
   local _pod_yaml="${_TEST_MANIFESTS_DIR}/pod-1.yaml"
   local _pod_name=$( yq '.metadata.name' "${_pod_yaml}" )
+  local _pvca_name=$( yq '.metadata.name' "${_pvca_yaml}" )
   local _pod_path=$( yq '.spec.containers[0].volumeMounts[0].mountPath' "${_pod_yaml}" )
   local _pvc_name=$( yq '.metadata.name' "${_pvc_yaml}" )
   local _namespace=$( yq '.metadata.namespace // "default"' "${_pod_yaml}" )
 
   _msg_info "starting test: consume space and resize"
-  _msg_info "creating test pvc and pod ..."
+  _msg_info "creating test pvc, pvca and pod ..."
   kubectl create -f "${_pvc_yaml}"
+  kubectl create -f "${_pvca_yaml}"
   kubectl create -f "${_pod_yaml}"
 
   _msg_info "waiting for test pod to be ready ..."
   kubectl wait "pod/${_pod_name}" \
           --for condition=Ready \
+          --namespace "${_namespace}" \
+          --timeout 10m
+
+  _msg_info "waiting for PVC Autoscaler resource to become healthy ..."
+  kubectl wait "pvca/${_pvca_name}" \
+          --for condition=Healthy \
           --namespace "${_namespace}" \
           --timeout 10m
 
@@ -57,6 +66,12 @@ function _test_consume_space_and_resize() {
 
   # We should be at 2Gi now
   _ensure_pvc_capacity "${_pvc_name}" "${_namespace}" 2Gi
+
+  _msg_info "waiting for PVC Autoscaler resource to become healthy ..."
+  kubectl wait "pvca/${_pvca_name}" \
+          --for condition=Healthy \
+          --namespace "${_namespace}" \
+          --timeout 10m
 
   _msg_info "consuming another 900MB of space ..."
   env POD="${_pod_name}" \
@@ -89,26 +104,41 @@ function _test_consume_space_and_resize() {
 
   # We should remain at 3Gi
   _ensure_pvc_capacity "${_pvc_name}" "${_namespace}" 3Gi
+
+  _msg_info "waiting for PVC Autoscaler resource to become unhealthy ..."
+  kubectl wait "pvca/${_pvca_name}" \
+          --for condition=Healthy=false \
+          --namespace "${_namespace}" \
+          --timeout 10m
 }
 
 # Tests the PVC autoscaler by consuming inodes from a volume and then expects
 # the PVC autoscaler to resize the volume.
 function _test_consume_inodes_and_resize() {
   local _pvc_yaml="${_TEST_MANIFESTS_DIR}/pvc-2.yaml"
+  local _pvca_yaml="${_TEST_MANIFESTS_DIR}/pvca-2.yaml"
   local _pod_yaml="${_TEST_MANIFESTS_DIR}/pod-2.yaml"
   local _pod_name=$( yq '.metadata.name' "${_pod_yaml}" )
+  local _pvca_name=$( yq '.metadata.name' "${_pvca_yaml}" )
   local _pod_path=$( yq '.spec.containers[0].volumeMounts[0].mountPath' "${_pod_yaml}" )
   local _pvc_name=$( yq '.metadata.name' "${_pvc_yaml}" )
   local _namespace=$( yq '.metadata.namespace // "default"' "${_pod_yaml}" )
 
   _msg_info "starting test: consume inodes and resize"
-  _msg_info "creating test pvc and pod ..."
+  _msg_info "creating test pvc, pvca and pod ..."
   kubectl create -f "${_pvc_yaml}"
+  kubectl create -f "${_pvca_yaml}"
   kubectl create -f "${_pod_yaml}"
 
   _msg_info "waiting for test pod to be ready ..."
   kubectl wait "pod/${_pod_name}" \
           --for condition=Ready \
+          --namespace "${_namespace}" \
+          --timeout 10m
+
+  _msg_info "waiting for PVC Autoscaler resource to become healthy ..."
+  kubectl wait "pvca/${_pvca_name}" \
+          --for condition=Healthy \
           --namespace "${_namespace}" \
           --timeout 10m
 
@@ -134,6 +164,12 @@ function _test_consume_inodes_and_resize() {
 
   # We should be at 2Gi now
   _ensure_pvc_capacity "${_pvc_name}" "${_namespace}" 2Gi
+
+  _msg_info "waiting for PVC Autoscaler resource to become healthy ..."
+  kubectl wait "pvca/${_pvca_name}" \
+          --for condition=Healthy \
+          --namespace "${_namespace}" \
+          --timeout 10m
 
   # After the first increase of the volume we should have a total of ~ 130K inodes.
   _msg_info "consuming another 60K inodes ..."
@@ -167,10 +203,23 @@ function _test_consume_inodes_and_resize() {
 
   # We should remain at 3Gi
   _ensure_pvc_capacity "${_pvc_name}" "${_namespace}" 3Gi
+
+  _msg_info "waiting for PVC Autoscaler resource to become unhealthy ..."
+  kubectl wait "pvca/${_pvca_name}" \
+          --for condition=Healthy=false \
+          --namespace "${_namespace}" \
+          --timeout 10m
 }
 
 # Main entrypoint
 function _main() {
+  _msg_info "Waiting for pvc-autoscaler pods to become ready ..."
+  kubectl wait \
+          --for condition=Ready \
+          --all Pod \
+          --namespace pvc-autoscaler-system \
+          --timeout 10m
+
   local _has_failed="false"
   if ! _test_consume_space_and_resize; then
     _msg_error "test consume space and resize has failed" 0
