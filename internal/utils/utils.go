960e2c35bae5b61efe024ee2ed20264b926c3d8b
Plamen Kokanov <plamen.kokanov@sap.com>
2026-08-21 14:46:51 +0300

    Add scheduling gate constant and Pod scheduling-gate helpers
    Introduces the offline-resize scheduling gate name and Has/Add/Remove
    helpers used by the upcoming offline-resize recovery controller and Pod
    mutating webhook.
    
    Signed-off-by: Plamen Kokanov <plamen.kokanov@sap.com>

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,6 +6,7 @@ package utils
 
 import (
 	"errors"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -52,4 +53,30 @@ func IsPersistentVolumeClaimConditionPresentAndEqual(obj *corev1.PersistentVolum
 	}
 
 	return false
+}
+
+// HasSchedulingGate returns whether the given Pod already has a scheduling gate
+// with the given name in its spec.
+func HasSchedulingGate(pod *corev1.Pod, name string) bool {
+	return slices.ContainsFunc(pod.Spec.SchedulingGates, func(gate corev1.PodSchedulingGate) bool {
+		return gate.Name == name
+	})
+}
+
+// AddSchedulingGate adds a scheduling gate with the given name to the Pod's spec,
+// if it is not already present.
+func AddSchedulingGate(pod *corev1.Pod, name string) {
+	if HasSchedulingGate(pod, name) {
+		return
+	}
+
+	pod.Spec.SchedulingGates = append(pod.Spec.SchedulingGates, corev1.PodSchedulingGate{Name: name})
+}
+
+// RemoveSchedulingGate removes the scheduling gate with the given name from the
+// Pod's spec if it is present.
+func RemoveSchedulingGate(pod *corev1.Pod, name string) {
+	pod.Spec.SchedulingGates = slices.DeleteFunc(pod.Spec.SchedulingGates, func(gate corev1.PodSchedulingGate) bool {
+		return gate.Name == name
+	})
 }
