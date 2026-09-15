833abd7cf97b5b0f01d870a73bdfeec3974600b3
Plamen Kokanov <plamen.kokanov@sap.com>
2026-09-15 12:59:44 +0300

    Address review comments
    Signed-off-by: Plamen Kokanov <plamen.kokanov@sap.com>

--- internal/utils/offline_resize.go
+++ internal/utils/offline_resize.go
@@ -8,7 +8,7 @@ import "github.com/gardener/pvc-autoscaler/api/autoscaling/v1alpha1"
 
 // IsOfflineResizeEnabled reports whether the given PersistentVolumeClaimAutoscaler
 // and the VolumePolicy that applies to a PVC it manages opt into offline-resize
-// recovery. This requires that both both the policy's scaleUp.resizeStrategy is PreferInPlace,
+// recovery. This requires that both the policy's scaleUp.resizeStrategy is PreferInPlace,
 // and the PVCA targets a workload controller rather than a PersistentVolumeClaim directly.
 func IsOfflineResizeEnabled(pvca *v1alpha1.PersistentVolumeClaimAutoscaler, policy *v1alpha1.VolumePolicy) bool {
 	return pvca != nil &&
