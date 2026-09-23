6fba790046e7f996dd4e969db9d688f30fc0854d
Rada Dimitrova <rada.dimitrova@sap.com>
2026-09-23 14:30:39 +0300

    Cleanup utils package
    Signed-off-by: Rada Dimitrova <rada.dimitrova@sap.com>

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -12,7 +12,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/Masterminds/semver/v3"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -22,40 +21,6 @@ import (
 // ErrBadPercentageValue is an error which is returned when attempting to parse
 // a bad percentage value.
 var ErrBadPercentageValue = errors.New("bad percentage value")
-
-// constraintK8sGreaterEqual134 matches Kubernetes versions 1.34.0 and higher.
-var constraintK8sGreaterEqual134 = mustNewConstraint(">= 1.34-0")
-
-func mustNewConstraint(constraint string) *semver.Constraints {
-	c, err := semver.NewConstraint(constraint)
-	if err != nil {
-		panic(err)
-	}
-
-	return c
-}
-
-// normalizeVersion returns the normalized version string by removing the
-// leading 'v' and any suffixes like '-rc1', '+build', etc.
-func normalizeVersion(version string) string {
-	v := strings.ReplaceAll(version, "v", "")
-	if idx := strings.IndexAny(v, "-+"); idx != -1 {
-		v = v[:idx]
-	}
-
-	return v
-}
-
-// IsKubernetesVersionGreaterEqual134 reports whether the given Kubernetes
-// version string is 1.34.0 or newer.
-func IsKubernetesVersionGreaterEqual134(version string) bool {
-	v, err := semver.NewVersion(normalizeVersion(version))
-	if err != nil {
-		return false
-	}
-
-	return constraintK8sGreaterEqual134.Check(v)
-}
 
 // ParsePercentage parses a string value, which represents percentage, e.g. 10%.
 func ParsePercentage(s string) (float64, error) {
@@ -99,11 +64,7 @@ func IsPersistentVolumeClaimConditionPresentAndEqual(obj *corev1.PersistentVolum
 // the given PersistentVolumeClaim has been rejected by the CSI driver as infeasible.
 func IsPersistentVolumeClaimResizeInfeasible(obj *corev1.PersistentVolumeClaim) bool {
 	status, ok := obj.Status.AllocatedResourceStatuses[corev1.ResourceStorage]
-	if !ok {
-		return false
-	}
-
-	return status == corev1.PersistentVolumeClaimControllerResizeInfeasible || status == corev1.PersistentVolumeClaimNodeResizeInfeasible
+	return ok && status == corev1.PersistentVolumeClaimControllerResizeInfeasible || status == corev1.PersistentVolumeClaimNodeResizeInfeasible
 }
 
 // FindOwningPVCAAndPolicy returns the PersistentVolumeClaimAutoscaler that manages the
